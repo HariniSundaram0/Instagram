8f186a716ced6867598b0c50f5721a8d3c9bbfa2
Harini Sundaram <harinisundaram@fb.com>
2022-06-27 11:31:57 -0700

    initial commit

---
+++ Instagram/AppDelegate.m
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.m
+//  Instagram
+//
+//  Created by Harini Sundaram on 6/27/22.
+//
+
+#import "AppDelegate.h"
+#import "Parse/Parse.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+            configuration.applicationId = @"0HenCYsWNntKrdJa9QMfKwdOsTFHUd1FEMj3d50D"; // <- UPDATE
+            configuration.clientKey = @"4m6oJ7Ceo7LYcMEQbZ3q9TDfrP9JAK2cqqphhbxo"; // <- UPDATE
+            configuration.server = @"https://parseapi.back4app.com";
+        }];
+
+        [Parse initializeWithConfiguration:config];
+
+        return YES;
+    return YES;
+}
+
+
+#pragma mark - UISceneSession lifecycle
+
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+
+@end
